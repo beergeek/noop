16a2ed84036a59574578762dd8bfbc93894116b1
beergeek <brett@puppet.com>
2017-09-19 08:52:14 +1000

    initial commit

--- tasks/init.rb
+++ tasks/init.rb
@@ -1,5 +1,4 @@
 #!/opt/puppetlabs/puppet/bin/ruby
-require 'json'
 require 'open3'
 require 'puppet'
 
@@ -7,10 +6,12 @@ def enable()
   cmd_string = "puppet config set noop true --section agent"
   _stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, stderr if status != 0
+  { status: status }
 end
 
 def disable()
   cmd_string = "puppet config set noop false --section agent"
   _stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, stderr if status != 0
+  { status: status }
 end
